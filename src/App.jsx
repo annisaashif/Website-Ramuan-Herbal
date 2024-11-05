2b4282e9e17b88c6791bf00800cb99a9fe1409f4
ashifaanis6@gmail.com <ashifaanis6@gmail.com>
2024-11-05 12:51:50 +0700

    Update Ketiga

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Kategori from './KatalogTanaman/Kategori';
 import DetailKategori from './KatalogTanaman/DetailKategori'; // Pastikan path file benar
+import TanamanDetail from './KatalogTanaman/TanamanDetail';
 
 function App() {
   return (
@@ -8,6 +9,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Kategori />} />
         <Route path="/kategori/:id" element={<DetailKategori />} />
+         {/* Tambahkan rute untuk halaman detail tanaman */}
+         <Route path="/tanaman/:name" element={<TanamanDetail />} />
       </Routes>
     </Router>
   );
